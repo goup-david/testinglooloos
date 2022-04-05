829db96f19e72a0776cd8d0899f30d1476e3a816
Matt Durey <matt.durey@goup.co.uk>
2022-04-05 11:08:35 +0100

    app.js to application.js

--- api/bookaloo/_js.php
+++ api/bookaloo/_js.php
@@ -5,4 +5,4 @@
 <script src="https://bookaloo.co/js/jquery/jquery.ui.js"></script>
 <script src="https://bookaloo.co/js/jquery/jquery.visible.js"></script>
 
-<script src="https://bookaloo.co/js/app.js?v=<?php echo time(); ?>"></script>
+<script src="https://bookaloo.co/js/application.js?v=<?php echo time(); ?>"></script>
