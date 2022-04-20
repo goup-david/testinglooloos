5980e0a15b22f24ed289bd5ba2c201320293f59c
goup-david <49403907+goup-david@users.noreply.github.com>
2022-04-20 12:34:18 +0100

    Update _navbar.php

--- api/_navbar.php
+++ api/_navbar.php
@@ -3,7 +3,7 @@
     <div class="container">
 
         <a class="navbar-brand transition-link" href="//looloos.co">
-            <img src="//looloos.co/img/brand/brand.svg" width="150" alt="looloos">
+            <img src="/img/brand/brand.svg" width="150" alt="looloos">
         </a>
 
         <button class="navbar-toggler" type="button">
@@ -15,14 +15,14 @@
         <div class="navbar-collapse">
 
             <ul class="navbar-nav">
-                <li class="<?php if($nav == 1) echo 'active' ?>"><a href="//looloos.co/commercial-portable-toilet-hire" class="transition-link">Commercial Hire</a></li>
-                <li class="<?php if($nav == 2) echo 'active' ?>"><a href="//looloos.co/private-portable-toilet-hire" class="transition-link">Private Hire</a></li>
-                <li class="<?php if($nav == 3) echo 'active' ?>"><a href="//looloos.co/contact" class="transition-link">Contact &amp; Help</a></li>
+                <li class="<?php if($nav == 1) echo 'active' ?>"><a href="/commercial-portable-toilet-hire" class="transition-link">Commercial Hire</a></li>
+                <li class="<?php if($nav == 2) echo 'active' ?>"><a href="/private-portable-toilet-hire" class="transition-link">Private Hire</a></li>
+                <li class="<?php if($nav == 3) echo 'active' ?>"><a href="/contact" class="transition-link">Contact &amp; Help</a></li>
             </ul>
 
             <ul class="navbar-nav right">
                 <li class="<?php if($nav == 4) echo 'active' ?>">
-                    <a href="//looloos.co/become-a-supplier" class="btn btn-white-o transition-link">Become a Supplier</a>
+                    <a href="/become-a-supplier" class="btn btn-white-o transition-link">Become a Supplier</a>
                 </li>
             </ul>
 
