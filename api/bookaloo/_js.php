cfd18d6a8fafc714496f3e327da1032fb90f3807
Matt Durey <matt.durey@goup.co.uk>
2022-04-27 13:30:41 +0100

    delete redundant code

--- api/bookaloo/_js.php
+++
@@ -1,8 +0,0 @@
-<!-- Javascript -->
-<script src="//code.jquery.com/jquery-3.4.0.min.js"></script>
-<script src="https://bookaloo.co/js/jquery/jquery.easing.js"></script>
-<script src="https://bookaloo.co/js/jquery/jquery.swiper.js"></script>
-<script src="https://bookaloo.co/js/jquery/jquery.ui.js"></script>
-<script src="https://bookaloo.co/js/jquery/jquery.visible.js"></script>
-
-<script src="https://bookaloo.co/js/application.js?v=<?php echo time(); ?>"></script>
